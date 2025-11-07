e077fec4930a929698ebbb708a8e9ebdcf8d54d4
Sudeep  Shivashettar <109416576+ShettyBro@users.noreply.github.com>
2025-11-07 19:37:16 +0530

    js update

--- public/js/myRecipes.js
+++ public/js/myRecipes.js
@@ -1,15 +1,7 @@
 // myRecipes.js
 let selectedImageFile = null;
 
-// Check authentication
-        function checkAuth() {
-            const token = localStorage.getItem('token');
-            if (!token) {
-                window.location.href = 'login.html';
-                return false;
-            }
-            return true;
-        }
+
 
         // Render recipes
         function renderRecipes() {
